05dd3e87868204c319dc37671e3d658b3210cfd1
Utkarsh Kumar Sharma <utkarshkviim@gmail.com>
2023-08-15 17:16:40 +0530

    initial code
    Signed-off-by: Utkarsh Kumar Sharma <utkarshkviim@gmail.com>

--- API/main.py
+++ API/main.py
@@ -32,4 +32,4 @@ def getJobByEmployer(employer_name: str):
 # Run the FastAPI app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
